928a53afa6ec98079f727bda8650796ae2f4036d
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-08-05 11:50:33 -0400

    calculations are correct

--- app.js
+++ app.js
@@ -3,15 +3,80 @@ name = prompt("Welcome to 21 Blackjack. To play, enter you name.")
 document.querySelector("h2").innerText = `Welcome ${name}. You are Player 1. Have Fun and Good luck!` 
 */
 
+let playersCards = []
+let playersTotal = 0
+let dealersCards = []
+let dealersTotal = 0
 
 
-/* Callback Functions */
 const hitButton = (event) => {
     const $hit = $("#hit")
-
+    let index = Math.floor(Math.random() * 52)
+    playersCards.push(deck[index])
+    renderCards()
 } 
 
+const newGameButton = (event) => {
+    playersCards = []
+    dealersCards = []
+    let index = Math.floor(Math.random() * 52)
+    playersCards.push(deck[index])
+    index = Math.floor(Math.random() * 52)
+    playersCards.push(deck[index])
 
+    index = Math.floor(Math.random() * 52)
+    dealersCards.push(deck[index])
+    index = Math.floor(Math.random() * 52)
+    dealersCards.push(deck[index])
+    renderCards()
+}
+
+const standButton = (event) => {
+    const $stand = $("#stand")
+    calDealersTotal()
+}
+
+const renderCards = () => {
+    let str = ""
+    for (let i =0; i <playersCards.length; i++) {
+       str+= playersCards[i].name + " "
+    }
+    $("#playersCards").text(str)
+
+    str = ""
+    for (let i =0; i <dealersCards.length; i++) {
+       str+= dealersCards[i].name + " "
+    }
+    $("#dealersCards").text(str)
+
+    calPlayersTotal()
+}
+
+const calPlayersTotal = () => {
+    playersTotal = 0
+    for (let i =0; i <playersCards.length; i++) {
+        playersTotal+= playersCards[i].value 
+    }
+    console.log(playersTotal)
+    // checkScore()
+}
+
+const calDealersTotal = () => {
+    dealersTotal = 0
+    for (let i =0; i <dealersCards.length; i++) {
+        dealersTotal+= dealersCards[i].value 
+    }
+    console.log(dealersTotal)
+//     checkScore()
+}
+
+// const checkScore = () => {
+//     if (playersTotal > 21){
+//         console.log("Bust: You lose!")
+//     else if (playersTotal > dealersTotal) {   
+//         console.log("You win!")
+//     }
+// }
 
 
 
@@ -19,5 +84,10 @@ const hitButton = (event) => {
 /* On load */
 $(() => {
     $("#hit").on("click", hitButton)
+    $("#newGame").on("click", newGameButton)
+    $("#stand").on("click", standButton)
+
 
 })
+
+
--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-/* Variables */
+/********** Cards/Deck *********/
 const values = [
     {name: "Ace", value: 1},
     {name: "Two", value: 2},
@@ -22,30 +22,17 @@ const suits = [
     {name: "Spades", value: 0}
 ];
 
-
-/* Cards */
-for (let key of values) {
-    for (let property of suits) {
-        console.log(`${key.name} of ${property.name}`)
+// // const createCards = () =>{
+    const deck =[]
+    for (let key of values) {
+        for (let property of suits) {
+            const cards = (`${key.name} of ${property.name}`)
+            // console.log(cards)
+            const valueofCards = key.value  
+            // console.log(valueofCards)
+            deck.push({name: cards, value: valueofCards})
+        }
     }
-}
-
-/* Deck */
-
-/* Shuffle Deck */
-
-/* Pull 2 cards for player, evaluate their sum and display their total value. If total value of player's hand is over 21, players loses. If value of player's hand is 21, player wins. If value of player's hand is less than 21, player will have option to hit or stand. If stand, sum of player's hand will be valuated against sum of dealer's hand. The person closest to 21 wins the game. If hit, 1 card is pulled from the deck and it's value added to players's total valu cards for player, evaluate their sum and display their total value.
-
-
-, sum of player's hand will be valuated against sum of dealer's hand. The person closest to 21 wins the game.
-
-/* Pull 2 cards for dealer, evaluate their sum and display their total value.
-    If value of dealer's hand is 17 or more, dealer must stand. If value of dealer's hand is 16 or less, dealer must draw another card from deck amd have it's value added to currrent hand's value */
-    
-
-
-
-
-
+// }
 
 
