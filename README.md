f23ce93a29ef102ea9be4807aa9852b0ec238217
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-08-06 09:36:12 -0400

    project completed

---
+++ README.md
@@ -0,0 +1,15 @@
+Project #1: The Game -> Twenty-One
+
+Technologies used:
+    - Wireframing, HTML, CSS, Javascript and GitHub
+Approach taken:
+    - I originally attempted to code game in the way in which it's played, meaning I tried to code the newgameButton first, then the hitButton, and so on and for forth, but I hit alot of obstacles with that approach. At different times it was easier to code certain parts of the game over others based on what I had already coded. Over time I realized it made more sense to code the game in an intuitive manner as ideas arouse intuitively, not by force. Once I switched my approach to an intuitive method I expreienced more inspiration as I coded along and less roadblocks.
+Link to live site:
+Unsolved problems:
+    - Game shows bust, win and lose alerts before showing pulled card and new total
+Future updates
+    - Game shows pulled card and new total before alerting user of bust, win or lose 
+    - Show images of cards pulled 
+    - Betting feature whereby score is reduced if dealer or player loses
+    - Reset button versus having to refresh page 
+
--- app.js
+++ app.js
@@ -7,6 +7,8 @@ let playersCards = []
 let playersTotal = 0
 let dealersCards = []
 let dealersTotal = 0
+let dealersScore = 0
+let playersScore = 0
 
 
 const hitButton = (event) => {
@@ -24,26 +26,33 @@ const newGameButton = (event) => {
     index = Math.floor(Math.random() * 52)
     playersCards.push(deck[index])
 
+    // index = Math.floor(Math.random() * 52)
+    // dealersCards.push(deck[index])
+    // index = Math.floor(Math.random() * 52)
+    // dealersCards.push(deck[index])
+    renderCards()
+  
+}
+
+const standButton = (event) => {
+    const $stand = $("#stand")
     index = Math.floor(Math.random() * 52)
     dealersCards.push(deck[index])
     index = Math.floor(Math.random() * 52)
     dealersCards.push(deck[index])
     renderCards()
-}
-
-const standButton = (event) => {
-    const $stand = $("#stand")
     calDealersTotal()
+  
 }
 
 const renderCards = () => {
-    let str = ""
+    let str = " "
     for (let i =0; i <playersCards.length; i++) {
        str+= playersCards[i].name + " "
     }
     $("#playersCards").text(str)
 
-    str = ""
+    str = " "
     for (let i =0; i <dealersCards.length; i++) {
        str+= dealersCards[i].name + " "
     }
@@ -58,7 +67,9 @@ const calPlayersTotal = () => {
         playersTotal+= playersCards[i].value 
     }
     console.log(playersTotal)
-    // checkScore()
+    $("#playersTotal").text(playersTotal)
+    checkBust()
+    // resetGame ()
 }
 
 const calDealersTotal = () => {
@@ -67,18 +78,65 @@ const calDealersTotal = () => {
         dealersTotal+= dealersCards[i].value 
     }
     console.log(dealersTotal)
-//     checkScore()
+    $("#dealersTotal").text(dealersTotal)
+    checkScore()
+    // resetGame ()
 }
 
-// const checkScore = () => {
-//     if (playersTotal > 21){
-//         console.log("Bust: You lose!")
-//     else if (playersTotal > dealersTotal) {   
-//         console.log("You win!")
+// const dealerPicksACard = () => {
+//     while (dealersTotal < 17){
+//         index = Math.floor(Math.random() * 52)
+//         dealersCards.push(deck[index])
+//         str = ""
+//         for (let i =0; i <dealersCards.length; i++) {
+//            str+= dealersCards[i].name + " "
+//         }
+//         $("#dealersCards").text(str)
 //     }
 // }
 
+const checkBust = () => {
+    let confirmed = ""
+    if (playersTotal > 21){
+        confirmed = window.confirm(`Bust! You have ${playersTotal}! You lose. Better luck next time!`);
+        dealersScore++
 
+    }  
+    if(confirmed) {
+        resetGame(); 
+      }
+}
+  
+const checkScore =  () => {
+    let confirmed = ""
+    if (dealersTotal === playersTotal){
+        confirmed = window.confirm(`You both have ${dealersTotal}! Tie game!`)
+
+    } else if (dealersTotal > playersTotal) {
+        confirmed = window.confirm(`The dealer has ${dealersTotal} and you have ${playersTotal}! Dealer wins! Better luck next time!`)
+        dealersScore++
+    } else {
+        confirmed = window.confirm(`The dealer has ${dealersTotal} and you have ${playersTotal}! You won! Great job!`)
+        playersScore++
+    }
+
+    if(confirmed) {
+      resetGame(); 
+    }
+}
+
+const resetGame = () => {
+    playersTotal = 0
+    dealersTotal = 0 
+    playersCards = []
+    dealersTotal = []
+    $("#dealersTotal").text("")
+    $("#playersTotal").text("")
+    $("#playersCards").text("")
+    $("#dealersCards").text("")
+    $("#dealersScore").text(dealersScore)
+    $("#playersScore").text(playersScore)
+}
 
 
 /* On load */
@@ -86,6 +144,7 @@ $(() => {
     $("#hit").on("click", hitButton)
     $("#newGame").on("click", newGameButton)
     $("#stand").on("click", standButton)
+    
 
 
 })
